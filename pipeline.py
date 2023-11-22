d9c9f6d18762bf1e0d9dbd4edcd9dd9220658cb3
Matteo-Negro <matteo3.negro@mail.polimi.it>
2023-11-22 09:30:03 -0600

    Beta pipeline

---
+++ pipeline.py
@@ -0,0 +1,349 @@
+import os
+import cv2
+import copy
+import torch
+
+import numpy as np
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+
+from PIL import Image
+from torch.optim import Adam
+from collections import defaultdict 
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from utils import SymbolCNN
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from sklearn.model_selection import train_test_split
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+
+model_path = '/Users/matteoblack/Desktop/Proj/visi-solve/cnn/CNN_full_model_py.pth'
+PATH = '/Users/matteoblack/Desktop/Proj/visi-solve/dataset'
+NUM_EPOCHS = 30
+
+
+def __threshold(img, threshold):
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            img[i,j] = 1 if img[i,j] > threshold else 0
+
+
+def __cutting_image(img, upper_cut, bottom_cut, left_cut, right_cut, padding):
+    zeros_v = np.zeros((upper_cut - bottom_cut, padding)) + 255
+    zeros_h = np.zeros((padding, right_cut - left_cut + 2 * padding)) + 255
+
+    symbol = img[bottom_cut:upper_cut, left_cut:right_cut]
+    symbol = np.c_[symbol, zeros_v]
+    symbol = np.c_[zeros_v, symbol]
+    symbol = np.vstack([symbol, zeros_h])
+    symbol = np.vstack([zeros_h, symbol])
+
+    return symbol
+
+
+def noise_reduction_v1(img, iteration=5, dim_kernel=10, threshold=0.5):
+    __threshold(img, threshold)
+
+    for _ in range(iteration):
+        img = cv2.blur(img, (dim_kernel, dim_kernel))
+        __threshold(img, threshold)
+
+    plt.imshow(img) 
+    plt.savefig(f'why')
+    plt.clf()
+
+    return img
+
+
+def noise_reduction_v2():
+    ...
+
+
+def symbol_decomposition_v1(img, par1=2, par2=1, par3=0.0001, par4=0.001, par5=180, threshold_cluster=100, padding=20, cluster_distance_threshold=20):
+    symbols = list()
+    dst = np.float32(img)
+    dst = cv2.cornerHarris(dst, par1, par2, par3)
+    dst = cv2.dilate(dst, None)
+
+    # Threshold for an optimal value, it may vary depending on the image.
+    X = list()
+    max_value = dst.max()
+    for c in range(dst.shape[0]):
+        for r in range(dst.shape[1]):
+            if dst[c, r] > par4 * max_value:
+                X.append([r, c])
+
+    # The following bandwidth can be automatically detected using
+    X = np.array(X)
+    bandwidth = estimate_bandwidth(X, quantile = par5 / X.shape[0])
+
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(X)
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
+
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+
+    clusters = defaultdict(list)
+    for k in range(n_clusters_):
+        my_members = labels == k
+        if sum(my_members) > threshold_cluster:
+            clusters[int(cluster_centers[k][0])] = X[my_members]
+
+    keys = sorted(clusters.keys())
+
+    left_cut = min(clusters[keys[0]][:, 0]) - 5
+    upper_cut = max(X[:,1]) + 5
+    bottom_cut = min(X[:,1]) - 5
+
+    counter = 0
+    for i in range(len(keys) - 1):
+        if abs(keys[i] - keys[i + 1]) > cluster_distance_threshold:
+            max_val = max(clusters[keys[i]][:, 0])
+            min_val = min(clusters[keys[i + 1]][:, 0])
+            right_cut = (max_val + min_val) // 2
+
+            symbols.append(__cutting_image(img, upper_cut, bottom_cut, left_cut, right_cut, padding))
+
+            left_cut = right_cut
+            counter += 1
+
+    right_cut = max(clusters[keys[-1]][:, 0]) + 5
+
+    symbols.append(__cutting_image(img, upper_cut, bottom_cut, left_cut, right_cut, padding))
+
+    return symbols
+
+
+def symbol_decomposition_v2():
+    ...
+
+
+def make_prediction(symbols):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    labels = list()
+
+    for image in symbols:
+        image = Image.fromarray(image, 'L')
+
+        # Define a transform to normalize the data
+        transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
+            transforms.Resize((28, 28)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+
+        # Apply the same transformations as your dataset
+        transformed_image = transform(image)
+
+        # Unsqueeze to add a batch dimension
+        image_batch = transformed_image.unsqueeze(0).to(device)
+
+        # Load the model
+        loaded_model = torch.load(model_path)
+        loaded_model.eval()
+
+        # Get predictions from the model
+        with torch.no_grad():
+            outputs = loaded_model(image_batch)
+            _, predicted = torch.max(outputs, 1)
+            labels.append(predicted.item())
+            # print(predicted.item())
+    
+    return labels
+
+
+def compute_result(labels):
+    eq = ''
+    for l in labels:
+        if l == 10:
+            eq += '/'
+        elif l == 11:
+            eq += '-'
+        elif l == 12:
+            eq += '*'
+        elif l == 13:
+            eq += '+'
+        else:
+            eq += str(l)
+    
+    return eq, eval(eq)
+
+
+def prepare_model():
+    # Define a transform to normalize the data
+    transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((28, 28)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    # Create a dataset from the folder structure
+    dataset = ImageFolder(PATH, transform=transform)
+
+    # Split the dataset into training and testing sets (80/20 split)
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = train_test_split(dataset, train_size=train_size, test_size=test_size, random_state=42)
+
+    #combined_train_dataset = ConcatDataset([mnist_train_dataset, train_dataset])
+    #combined_test_dataset = ConcatDataset([mnist_test_dataset, test_dataset])
+
+    # Create DataLoader instances for training and testing
+    batch_size = 32 
+
+    from torch.utils.data.dataloader import default_collate
+    def custom_collate(batch):
+        batch = [(item[0], torch.tensor(item[1], dtype=torch.long)) if not isinstance(item[1], torch.LongTensor) else item for item in batch]
+        return default_collate(batch)
+
+    # Use the custom collate function in your DataLoader
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=custom_collate)
+    print('Number of training batches', len(train_loader))
+    print('Number of test batches', len(test_loader))
+
+    #print('Number of training samples', len(combined_train_dataset))
+    #print('Number of test samples', len(combined_test_dataset))
+
+    # Initialize the model
+    model = SymbolCNN.SymbolCNN()
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=0.001)
+
+    # Move the model to GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    print("Device is:", device)
+    print("-----------------------------------")
+
+    # Training the model
+
+    # Initialize lists to track the loss and accuracy
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
+    test_losses = []
+    test_accuracies = []
+
+    for epoch in range(NUM_EPOCHS):
+        model.train()  # Set the model to training mode
+        train_loss = 0.0
+        correct_train = 0
+        total_train = 0
+
+        for images, labels in train_loader:
+            # Move data to device
+            images, labels = images.to(device), labels.to(device)
+            
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+            
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            
+            # Backward pass and optimize
+            loss.backward()
+            optimizer.step()
+            
+            # Calculate training loss
+            train_loss += loss.item() * images.size(0)
+
+            # Calculate training accuracy
+            _, predicted = torch.max(outputs.data, 1)
+            total_train += labels.size(0)
+            correct_train += (predicted == labels).sum().item()
+        
+        # Print training statistics
+        train_loss = train_loss / len(train_loader.dataset)
+        train_accuracy = 100 * correct_train / total_train
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+
+        # Evaluate the model with the test data after training
+        model.eval()  # Set the model to evaluation mode
+        test_loss = 0.0
+        correct_test = 0
+        total_test = 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                test_loss += loss.item() * images.size(0)
+                _, predicted = torch.max(outputs.data, 1)
+                total_test += labels.size(0)
+                correct_test += (predicted == labels).sum().item()
+        # Calculate average test loss and accuracy
+        test_loss = test_loss / len(test_loader.dataset)
+        test_accuracy = 100 * correct_test / total_test
+        test_losses.append(test_loss)
+        test_accuracies.append(test_accuracy)
+
+
+        print(f'Epoch {epoch+1}/{NUM_EPOCHS} - Train loss: {train_loss:.4f} - Train Accuracy: {train_accuracy:.2f}% - Test Loss: {test_loss:.2f} - Test Accuracy: {test_accuracy:.2f}%')
+
+
+    # Save the entire model
+    torch.save(model, '/Users/matteoblack/Desktop/Proj/visi-solve/cnn/CNN_full_model.pth')
+
+    # Evaluate the model with the test data after training
+    model.eval()  # Set the model to evaluation mode
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Test Accuracy: {100 * correct / total:.2f}%')
+
+
+def print_symbols(symbols):
+    for counter, s in enumerate(symbols):
+        plt.imshow(s, cmap='gray')
+        plt.savefig(f'digit{counter}')
+        plt.clf()
+
+
+def main(filename):
+    eq = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2GRAY)
+
+    # eq = noise_reduction_v1(eq)
+
+    # symbols = symbol_decomposition_v1(eq)
+
+    symbols = list()
+    for s in sorted(os.listdir('/Users/matteoblack/Desktop/Proj/visi-solve/test_data/')):
+        if s != '.DS_Store': 
+            # print(s)
+            symbols.append(cv2.cvtColor(cv2.imread(f'/Users/matteoblack/Desktop/Proj/visi-solve/test_data/{s}'), cv2.COLOR_BGR2GRAY))
+
+    # print_symbols(symbols)
+
+    # prepare_model()
+
+    labels = make_prediction(symbols)
+
+    formula, result = compute_result(labels)
+
+    print(f'The result of {formula} is {result}.')
+
+
+if __name__ == "__main__":
+    main('/Users/matteoblack/Desktop/Proj/visi-solve/symbol-decomposition/output03.png')
